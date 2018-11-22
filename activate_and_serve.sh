7ac9c9208b1dda7ed0f392a3cb699df1500faaab
Jessica Kerr <jessitron@atomist.com>
2018-11-22 12:36:40 -0600

    also ruby

--- activate_and_serve.sh
+++ activate_and_serve.sh
@@ -6,6 +6,7 @@ function die(){
 }
 
 . ~/.venvs/userdocs/bin/activate || die "virtual env activation failed"
+eval "$(rbenv init -)" || die "rbenv failed"
 pip install -r requirements.txt || die "pip install failed"
 mkdocs build --strict || die "mkdocs build failed"
 ./htmlproof.sh || die "HTMLProofer failed"
